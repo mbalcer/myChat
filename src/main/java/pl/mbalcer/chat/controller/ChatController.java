b65f473e50e2364c762f06e0e9950c175ef2d37b
mbalcer <mateusz75319@gmail.com>
2019-10-15 00:03:00 +0200

    Add chat controller

---
+++ src/main/java/pl/mbalcer/chat/controller/ChatController.java
@@ -0,0 +1,16 @@
+package pl.mbalcer.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import pl.mbalcer.chat.model.Message;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public Message getMessage(Message message) {
+        return message;
+    }
+}
