6e6e5aace7927e2462ccbc5deeb773b8de58c658
mbalcer <mateusz75319@gmail.com>
2019-10-16 14:06:02 +0200

    Cleared default configuration

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'chat'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('chat');
   });
 
   it('should render title in a h1 tag', () => {
