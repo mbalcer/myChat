c13c54eb250d8298eac09c46e8da8f7660bf5ab7
mbalcer <mateusz75319@gmail.com>
2019-10-18 19:49:47 +0200

    The variables attached to the view

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Message} from "./model/message";
+import {User} from "./model/user";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Chat';
+export class AppComponent implements OnInit{
+  messages: Message[] = [];
+  user: string;
+  yourMessage: string;
+
+  constructor () {}
+
+  ngOnInit() {}
+
+  sendMessage() {
+
+  }
 }
