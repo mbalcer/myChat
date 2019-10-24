1ade4947bd6d21657046048f68ac38e3c12dc39d
mbalcer <mateusz75319@gmail.com>
2019-10-24 21:59:48 +0200

    Added header web component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderWebComponent } from './header-web/header-web.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderWebComponent
   ],
   imports: [
     BrowserModule,
