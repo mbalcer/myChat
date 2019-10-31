262d8cab35375df72c2558f68ad6d0a261d39f0f
mbalcer <mateusz75319@gmail.com>
2019-10-31 21:50:01 +0100

    Created login / register panel

--- frontend/src/app/login-panel/login-panel.component.ts
+++ frontend/src/app/login-panel/login-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-login-panel',
@@ -12,4 +13,7 @@ export class LoginPanelComponent implements OnInit {
   ngOnInit() {
   }
 
+  togglePanel() {
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+  }
 }
