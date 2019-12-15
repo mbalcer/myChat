2e034851a34b573ee8d1e2232b1fe7f15b5d4b86
mbalcer <mateusz75319@gmail.com>
2019-12-16 00:03:43 +0100

    Added elements to info-panel: user icon, welcome to user and button to sign out

--- frontend/src/app/chat/info-panel/info-panel.component.ts
+++ frontend/src/app/chat/info-panel/info-panel.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-info-panel',
@@ -6,8 +7,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./info-panel.component.css']
 })
 export class InfoPanelComponent implements OnInit {
+  user: User;
 
   constructor() {
+    this.user = {
+      login: "Mateusz",
+      password: null,
+      email: null,
+      color: null
+    }
   }
 
   ngOnInit() {
