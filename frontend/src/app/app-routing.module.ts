68bf4046f18a03d01ddf74f91922ce4ce64f0927
mbalcer <mateusz75319@gmail.com>
2020-01-27 23:15:46 +0100

    Plugged user profile component to routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {ChatComponent} from "./chat/chat.component";
 import {NeedAuthGuard} from "./service/need-auth-guard";
 import {LoginPanelComponent} from "./login-panel/login-panel.component";
+import {UserProfileComponent} from "./user-profile/user-profile.component";
 
 
 const routes: Routes = [
+  {
+    path: 'profile',
+    component: UserProfileComponent,
+    canActivate: [NeedAuthGuard]
+  },
   {
     path: 'chat',
     component: ChatComponent,
