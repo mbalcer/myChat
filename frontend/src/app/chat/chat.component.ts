49dd4e0bb1d479befd997c4f63a275605267a250
mbalcer <mateusz75319@gmail.com>
2020-03-03 22:39:45 +0100

    Changed displaying SYSTEM and HELP command for only per user

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -79,7 +79,11 @@ export class ChatComponent implements OnInit, AfterViewChecked, OnDestroy {
     client.connect({}, function(frame) {
       client.subscribe("/topic/" + room, function (message) {
         if (JSON.parse(message.body).room == that.room) {
-          that.showMessage(JSON.parse(message.body).user, JSON.parse(message.body).message, JSON.parse(message.body).dateTime, JSON.parse(message.body).type);
+          if (JSON.parse(message.body).type == 'MESSAGE') {
+            that.showMessage(JSON.parse(message.body).user, JSON.parse(message.body).message, JSON.parse(message.body).dateTime, JSON.parse(message.body).type);
+          } else if ((JSON.parse(message.body).type == 'HELP' || JSON.parse(message.body).type == 'SYSTEM') && JSON.parse(message.body).user.login == that.user.login) {
+            that.showMessage(JSON.parse(message.body).user, JSON.parse(message.body).message, JSON.parse(message.body).dateTime, JSON.parse(message.body).type);
+          }
         }
       });
     });
