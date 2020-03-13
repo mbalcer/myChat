6d3db4a46327b9538a058d19cf8be30332f78186
mbalcer <mateusz75319@gmail.com>
2020-03-13 14:40:12 +0100

    Created chat service

--- src/main/java/pl/mbalcer/chat/controller/ChatController.java
+++ src/main/java/pl/mbalcer/chat/controller/ChatController.java
@@ -4,64 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pl.mbalcer.chat.model.Message;
-import pl.mbalcer.chat.model.MessageType;
-import pl.mbalcer.chat.service.CommandService;
-import pl.mbalcer.chat.service.RoomService;
+import pl.mbalcer.chat.service.ChatService;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
 public class ChatController {
-
-    private List<Message> historyOfMessage = new ArrayList<>();
-    private CommandService commandService;
-    private SimpMessagingTemplate simpMessagingTemplate;
-    private RoomService roomService;
+    private ChatService chatService;
 
     @Autowired
-    public ChatController(CommandService commandService, SimpMessagingTemplate simpMessagingTemplate, RoomService roomService) {
-        this.commandService = commandService;
-        this.simpMessagingTemplate = simpMessagingTemplate;
-        this.roomService = roomService;
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
     }
 
     @MessageMapping("/chat/{room}")
     @SendTo("/topic/{room}")
     public Message getMessage(@DestinationVariable String room, Message message) {
-        message.setDateTime(LocalDateTime.now());
-        message = this.commandService.checkMessage(message);
-        if (message.getType().equals(MessageType.ALERT)) {
-            List<String> allRooms = roomService.getAllRooms();
-            allRooms.add("All");
-            for (String nameRoom : allRooms) {
-                if (!nameRoom.equals(room)) {
-                    Message copyMessage = new Message(message.getUser(), nameRoom, message.getDateTime(), message.getType(), message.getMessage());
-                    simpMessagingTemplate.convertAndSend("/topic/" + nameRoom, copyMessage);
-                    historyOfMessage.add(copyMessage);
-                }
-            }
-        }
-        historyOfMessage.add(message);
-        return message;
+        return chatService.processMessage(message, room);
     }
 
     @GetMapping("/api/chat/{room}/{login}")
     public List<Message> getMessagesByRooms(@PathVariable String room, @PathVariable String login) {
-        return historyOfMessage.stream()
-                .filter(r -> r.getRoom().equals(room))
-                .filter(m -> (m.getType().equals(MessageType.MESSAGE)) ||
-                        (m.getType().equals(MessageType.ALERT)) ||
-                        ((m.getType().equals(MessageType.SYSTEM) || m.getType().equals(MessageType.HELP) || m.getType().equals(MessageType.ERROR)) && m.getUser().getLogin().equals(login)))
-                .collect(Collectors.toList());
+        return chatService.getMessagesByRoomAndLogin(room, login);
     }
 }
---
+++ src/main/java/pl/mbalcer/chat/service/ChatService.java
@@ -0,0 +1,59 @@
+package pl.mbalcer.chat.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import pl.mbalcer.chat.model.Message;
+import pl.mbalcer.chat.model.MessageType;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ChatService {
+
+    private List<Message> historyOfMessage = new ArrayList<>();
+    private CommandService commandService;
+    private RoomService roomService;
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    public ChatService(CommandService commandService, RoomService roomService, SimpMessagingTemplate simpMessagingTemplate) {
+        this.commandService = commandService;
+        this.roomService = roomService;
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    public void addToHistory(Message message) {
+        this.historyOfMessage.add(message);
+    }
+
+    public Message processMessage(Message message, String room) {
+        message.setDateTime(LocalDateTime.now());
+        message = this.commandService.checkMessage(message);
+        if (message.getType().equals(MessageType.ALERT)) {
+            List<String> allRooms = roomService.getAllRooms();
+            allRooms.add("All");
+            for (String nameRoom : allRooms) {
+                if (!nameRoom.equals(room)) {
+                    Message copyMessage = new Message(message.getUser(), nameRoom, message.getDateTime(), message.getType(), message.getMessage());
+                    simpMessagingTemplate.convertAndSend("/topic/" + nameRoom, copyMessage);
+                    addToHistory(copyMessage);
+                }
+            }
+        }
+        addToHistory(message);
+        return message;
+    }
+
+    public List<Message> getMessagesByRoomAndLogin(String room, String login) {
+        return historyOfMessage.stream()
+                .filter(r -> r.getRoom().equals(room))
+                .filter(m -> (m.getType().equals(MessageType.MESSAGE)) ||
+                        (m.getType().equals(MessageType.ALERT)) ||
+                        ((m.getType().equals(MessageType.SYSTEM) || m.getType().equals(MessageType.HELP) || m.getType().equals(MessageType.ERROR)) && m.getUser().getLogin().equals(login)))
+                .collect(Collectors.toList());
+    }
+}
