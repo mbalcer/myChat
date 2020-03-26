2e7952831a431d705f76cb57deaeb08289f57e95
mbalcer <mateusz75319@gmail.com>
2020-03-26 23:38:26 +0100

    Created ban view

--- frontend/src/app/ban/ban.component.ts
+++ frontend/src/app/ban/ban.component.ts
@@ -3,6 +3,7 @@ import {TokenService} from "../service/token.service";
 import {BanService} from "../service/ban.service";
 import {Ban} from "../model/ban";
 import {Router} from "@angular/router";
+import {formatDate} from "@angular/common";
 
 @Component({
   selector: 'app-ban',
@@ -18,12 +19,19 @@ export class BanComponent implements OnInit {
     this.banService.getBanByUser(this.userLogin).subscribe(n => {
       if (n == null)
         this.router.navigateByUrl("/chat");
-      else
+      else {
         this.ban = n;
+        this.ban.end = formatDate(n.end, 'dd.MM.yyyy HH:mm', 'en');
+      }
     });
   }
 
   ngOnInit() {
+  }
+
+  backToLogin() {
+    this.tokenService.removeToken();
+    this.router.navigateByUrl('');
   }
 
 }
