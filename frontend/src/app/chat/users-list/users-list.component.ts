f508c1c202c414c497aa02d72e24491b53562a06
mbalcer <mateusz75319@gmail.com>
2020-04-27 22:44:19 +0200

    Designed user list component

--- frontend/src/app/chat/users-list/users-list.component.ts
+++ frontend/src/app/chat/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-users-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list.component.css']
 })
 export class UsersListComponent implements OnInit {
+
+  users: User[] = [
+    {login: "jan123", password: "", email: "", color: "#123456", role: "user"},
+    {login: "nowcio23", password: "", email: "", color: "#abcdef", role: "user"},
+    {login: "jgaregaw23", password: "", email: "", color: "#432689", role: "user"},
+    {login: "sucahrex", password: "", email: "", color: "#312853", role: "user"},
+  ];
 
   constructor() { }
 
