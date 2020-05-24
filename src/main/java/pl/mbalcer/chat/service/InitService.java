19bbb415f3d814896d8281311020e3d9afabcfda
mbalcer <mateusz75319@gmail.com>
2020-05-24 22:52:23 +0200

    Fixed init service

--- src/main/java/pl/mbalcer/chat/service/InitService.java
+++ src/main/java/pl/mbalcer/chat/service/InitService.java
@@ -24,18 +24,20 @@ public class InitService {
 
     @PostConstruct
     public void init() {
-        User u1 = new User("Admin", "admin123", "admin@mychat.pl", "#00FFFF");
-        u1.setRole(Role.ADMIN);
-        User u2 = new User("Mateusz", "mati123", "mateusz75319@gmail.com", "#0000FF");
-        Room room = new Room(1l, "room1", Arrays.asList(u1, u2));
-        Room programming = new Room(2l, "Programming", Arrays.asList(u1));
+        if(userRepository.findById(1l).isPresent()) {
+            User u1 = new User("Admin", "admin123", "admin@mychat.pl", "#00FFFF");
+            u1.setRole(Role.ADMIN);
+            User u2 = new User("Mateusz", "mati123", "mateusz75319@gmail.com", "#0000FF");
+            Room room = new Room(1l, "room1", Arrays.asList(u1, u2));
+            Room programming = new Room(2l, "Programming", Arrays.asList(u1));
 
-        Ban b1 = new Ban(1l, LocalDateTime.now(), LocalDateTime.now().plusSeconds(30), BanType.BAN, u2);
+            Ban b1 = new Ban(1l, LocalDateTime.now(), LocalDateTime.now().plusSeconds(30), BanType.BAN, u2);
 
-        userRepository.save(u1);
-        userRepository.save(u2);
-        roomRepository.save(room);
-        roomRepository.save(programming);
-        banRepository.save(b1);
+            userRepository.save(u1);
+            userRepository.save(u2);
+            roomRepository.save(room);
+            roomRepository.save(programming);
+            banRepository.save(b1);
+        }
     }
 }
